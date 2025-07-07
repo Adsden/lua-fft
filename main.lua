723d5c1496e7c0b75faa2c08753294dec04461a6
Aiden Usui <aiden.usui6@gmail.com>
2025-07-07 13:25:33 +1000

    cleanup code and document

--- main.lua
+++ main.lua
@@ -21,37 +21,32 @@ end
 
 -- Locals
 
-local soundData ---@type love.SoundData
 local decoder ---@type love.Decoder
-local fft_data, comp_time_fft
 local source ---@type love.Source
+local soundData ---@type love.SoundData
+local fft_data, comp_time_fft
 
 function love.load()
-    -- local file = [[sin440Hz_44100Hz_1024samples.wav]]
-    local file = [[untitled.wav]]
-    -- soundData = love.sound.newSoundData(file)
+    -- create sound decoder and source
     decoder = love.sound.newDecoder("song1.mp3", 4096)
     source = love.audio.newSource(decoder, "stream")
     source:play()
-    -- initial data
-    soundData = decoder:decode()
-
-    fft_data, comp_time_fft = fft.naive_dft(soundData)
 end
 
 function love.update(dt)
+    -- Clone decoder and seek to current audio position
+    -- new decoder will have pos=0 which allows seeking to source's exact position
     local d = decoder:clone()
     d:seek(source:tell("seconds"))
-    soundData = d:decode()
 
+    -- get sound data at current pos and compute fft
+    soundData = d:decode()
     fft_data, comp_time_fft = fft.naive_dft(soundData)
-    -- source = love.audio.newSource(soundData)
-    -- source:play()
 end
 
 function love.draw()
+    -- Print debug info
     love.graphics.setColor(1, 1, 1)
-    -- Info
     DEBUG_PRINT_LINE = 0
     debug("%d samples @ %dHz", soundData:getSampleCount(), soundData:getSampleRate())
     debug("Window resolution: %d x %d", love.graphics.getDimensions())
@@ -69,22 +64,20 @@ function love.draw()
     love.graphics.points(points)
 
     -- frequency domain plot
-    local points = {}
-    local points2 = {}
+    points = {}
     for i = 0, love.graphics.getWidth() do
         table.insert(points, i)
-        table.insert(points2, i)
         local re, im = complex.get(fft_data[i])
         local magnitude = math.sqrt(re ^ 2 + im ^ 2)
         table.insert(points, centre_y - magnitude)
-        table.insert(points2, centre_y + magnitude)
     end
     love.graphics.setColor(0, 1, 0)
     love.graphics.line(points)
-    love.graphics.line(points2)
 end
 
 DEBUG_PRINT_LINE = 0
+-- Handles printing debug messages to screen on a new line.
+-- Uses string.format for more consise debug print statements
 function debug(fmt, ...)
     love.graphics.print(string.format(fmt, ...), 0, 13 * DEBUG_PRINT_LINE)
     DEBUG_PRINT_LINE = DEBUG_PRINT_LINE + 1
